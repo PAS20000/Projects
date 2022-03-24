1f13153fce417fe9207e35ae44b4527b87edd388
PAS818 <83708869+PAS19@users.noreply.github.com>
2022-03-24 19:44:01 -0300

    up

---
+++ app/next.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    images: {
+        domains: [
+            'lh3.googleusercontent.com',
+        ]
+    },
+    trailingSlash: true,
+    async headers(){
+        return [{
+            source:'/:path*',
+            headers:[{
+                key:'X-Frame-Options',
+                value:'SAMEORIGIN',
+               
+            },{
+                key:'x-robots-tag',
+                value:'all',
+            }, {
+                key:'x-robots-tag',
+                value:'*',
+            },
+            ]
+        }]
+    }
+}
