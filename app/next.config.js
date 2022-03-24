ea41c3a172e06ff7783bcb35871c41250ab2e278
PAS818 <83708869+PAS19@users.noreply.github.com>
2022-03-24 16:20:42 -0300

    Update next.config.js

--- app/next.config.js
+++ app/next.config.js
@@ -1,18 +1,26 @@
-export const images = {
-    domains: [],
-}
-export const trailingSlash = true
-export async function headers() {
-    return [{
-        source: '/:path*',
-        headers: [{
-            key: 'X-Frame-Options',
-            value: 'DENY',
-            
-        }, 
-        {
-            key: 'x-robots-tag',
-            value: 'all'
+module.exports = {
+    images: {
+        domains: [
+        'lh3.googleusercontent.com',
+        'media-exp1.licdn.com',
+        'avatars.githubusercontent.com'],
+    },
+    trailingSlash: true,
+    async headers(){
+        return [{
+            source:'/:path*',
+            headers:[{
+                key:'X-Frame-Options',
+                value:'SAMEORIGIN',
+               
+            },{
+                key:'x-robots-tag',
+                value:'all',
+            }, {
+                key:'x-robots-tag',
+                value:'*',
+            },
+            ]
         }]
-    }]
+    }
 }
