f434323cc4541d732d6cf509cff037765a374d71
PAS818 <83708869+PAS19@users.noreply.github.com>
2022-03-26 10:03:00 -0300

    banner

--- app/src/components/NextHead/NextHead.tsx
+++ app/src/components/NextHead/NextHead.tsx
@@ -12,11 +12,11 @@ const NextHead = ({ title, description, robots, googleBot, canonical }) => {
                 }
                 <meta property='og:type' content='website' key='website'/>
                 <meta property='og:locale' content='pt_BR' key='locale'/>
-                <meta property='og:url' content='https://projects-red.vercel.app/img/banner.png' key='url'/>
+                <meta property='og:url' content='https://projects-red.vercel.app' key='url'/>
                 <meta property='og:title' content={title} key='titleog'/>
                 <meta property='og:site_name' content='MVP' key='site_name'/>
                 <meta property='og:description' content={description} key='descriptionog'/>
-                <meta property='og:image' content='https://projects-red.vercel.app/img/banner.png' key='imageog'/>
+                <meta property='og:image' content='https://projects-pas19.vercel.app/img/banner.png' key='imageog'/>
                 <meta property='og:image:type' content='image/png' key='imageogpng'/>
                 <meta property='og:image:width' content='400' key='imageogpngwidth'/>
                 <meta property='og:image:height' content='300' key='imageogpngheight'/> 
