44eed694ad83ed05c3a138d22b072336e335f7d6
PAS818 <83708869+PS200000@users.noreply.github.com>
2022-04-11 20:33:59 -0300

    Update NextHead.tsx

--- app/src/components/NextHead/NextHead.tsx
+++ app/src/components/NextHead/NextHead.tsx
@@ -19,8 +19,8 @@ const NextHead = ({ title, description, robots, googleBot, canonical }) => {
                 <meta property='og:image' content='https://projects-red.vercel.app/img/banner.png' key='imageog'/>
                 <meta property='og:image:secure_url' content='https://projects-red.vercel.app/img/banner.png' key='imageogSecure'/>
                 <meta property='og:image:type' content='image/png' key='imageogpng'/>
-                <meta property='og:image:width' content='1300' key='imageogpngwidth'/>
-                <meta property='og:image:height' content='630' key='imageogpngheight'/> 
+                <meta property='og:image:width' content='600' key='imageogpngwidth'/>
+                <meta property='og:image:height' content='300' key='imageogpngheight'/> 
                 <meta name='application-name' content='Projects' key='appname'/>
                 <meta name='description' content={description} key='description'/>
                 <meta name='robots' content={robots} key='robots'/>
